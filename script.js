16a8472c2582e436cfe630057bf7e531cbf34e29
NIMCOO <nimco77001@gmail.com>
2023-12-07 00:25:46 +0000

    done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+const reverse = ["fikrcamp","bootcamp"]
+for(let i = reverse.length -1; i > 0; i--)
+console.log(`${i}.${reverse[i]}`);
+
+
+// hello there
+const rever = ["hello there!"]
+for(let i = rever.length -1; i > 0; i--)
+console.log(`${i}.${rever[i]}`);
+
+
+// welcome to my city
+const back = ["welcome to my city"]
+for(let i = back.length -1; i > 0; i--)
+console.log(`${i}.${back[i]}`);
+
